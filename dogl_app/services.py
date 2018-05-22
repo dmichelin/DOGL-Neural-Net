af65b7fc6f4c8df050e027a7ea21fae4d927bdfa
Daniel Michelin <k14dm01@kzoo.edu>
2018-05-21 20:13:24 -0400

    nearly there, sweet mother of god

---
+++ dogl_app/services.py
@@ -0,0 +1,29 @@
+import time
+import requests
+weather_predictions = None
+# Gets weather predictions then caches them to save on API calls. 
+def get_weather_predictions():
+    global weather_predictions
+    if weather_predictions is None:
+        # make a call
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q=south%20haven,US&appid=500054628f30fd9891b167999db911f8'
+        r = requests.get(url)
+        r = r.json()
+        full_predictions = r['list']
+        # Map to predicted rain in mm, predicted temp_max in fahrenheit, and date
+        useful_predictions = list(map(lambda entry: [entry['rain']['3h'] if '3h' in entry['rain'] else 0, convert_kelvin_to_fahrenheit(entry['main']['temp_max']), convert_epoch_day_to_excel_day(entry['dt']),get_day(entry['dt'])],full_predictions))
+        #Every fifth prediction is the middle of the day
+        weather_predictions = useful_predictions[4::8]
+    print(weather_predictions)
+    return weather_predictions
+
+        
+
+def convert_kelvin_to_fahrenheit(num):
+    return (9*num)/5 - 459.67
+def convert_epoch_day_to_excel_day(tm):
+    tm = time.gmtime(int(tm))
+    return (tm.tm_wday+1)%7+1
+def get_day(tm):
+    tm = time.gmtime(int(tm))
+    return tm.tm_mday
--- dogl_app/views.py
+++ dogl_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .DOGLNN import DoglNet
+from .services import get_weather_predictions
+from datetime import datetime
 def index(request):
     d = DoglNet()
     d.initModel()
-    return HttpResponse(d.predict([[0,78,5,23]]))
+    weather = get_weather_predictions()
+    predictions = d.predict(weather)
+    roundedPredictions = list(map(lambda x: round(x[1],4), predictions))
+    text = list(map(lambda x: "DOGL Probability of " + str(datetime.now().strftime('%B'))+" " + str(x[3]) + " is ",weather))
+    text_predictions = [text[i] + str(roundedPredictions[i]) for i in range(len(text))]
+    context = {'predictions': text_predictions}
+    return render(request, 'dogl/index.html',context)
+
